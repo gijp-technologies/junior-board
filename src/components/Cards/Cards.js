fc1fc553fb01d72f284d60873611c7e5c41d0960
unknown <ilyrodriguez@gmail.com>
2019-08-26 06:30:14 -0400

    card style not finished yet

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import Card from "react-bootstrap/Card";
 import './Style.css';
-// import JuniorDevs from "../API/JuniorDevs.json";
-
 const JuniorsDevs = require("../API/JuniorsDevs.json");
 
 class Juniors extends Component {
@@ -16,29 +14,30 @@ class Juniors extends Component {
 
     render() {
         return (
-            <div className="JrCard">
+            <div>
                 {JuniorsDevs.map((dev, key) =>
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: '28rem' }}>
                         <Card.Body>
-                            <row>
-
-                                <Card.Title>{dev.firstName + " " + dev.lastName}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">{dev.city + ", " + dev.state}</Card.Subtitle>
-                                <Card.Text>
-                                    Phone: {" " + dev.phone}
+                            <row className="card-top-section">
+                                {/* IMAGE GOES HERE: */}
+                                <span><div className="image-placeholder"></div>
+                                <Card.Title className="card-info" as="h4">{dev.firstName + " " + dev.lastName}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted card-info" as="h5">{dev.city + ", " + dev.state}</Card.Subtitle>
+                                <Card.Text className="card-info">
+                                Phone: {" " + dev.phone }  Email: {" " + dev.email}
                                 </Card.Text>
-                                <Card.Text>
-                                    Email: {" " + dev.email}
-                                </Card.Text>
+                                </span>
                             </row>
 
                             <row>
-                                <Card.Text>
-                                    Skills: {" " + dev.skillset}
+                                <Card.Text className="skillset">
+                                Skills: {" " + dev.skillset}
                                 </Card.Text>
-                                <Card.Link href={dev.link1} target="blank">Card Link</Card.Link>
-                                <Card.Link href={dev.link2} target="blank">Another Link</Card.Link>
-                                <Card.Link href={dev.link3} target="blank">Link 3</Card.Link>
+                                <div className="card-footer">
+                                <Card.Link  href={dev.link1} target="blank">Card Link</Card.Link>
+                                <Card.Link  href={dev.link2} target="blank">Another Link</Card.Link>
+                                <Card.Link  href={dev.link3} target="blank">Link 3</Card.Link>
+                                </div>
                             </row>
                         </Card.Body>
                     </Card>
