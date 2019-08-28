4257427022e2f45f1936576ff5a79a759c891f25
Greg Baker <semperfifcd@gmail.com>
2019-08-28 14:53:27 -0400

    Changed Navbar imports.

--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { Navbar, Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
 import "./Style.css";
 
 class Nav extends Component {
     render() {
         return (
             <Navbar className="nav" backgroundColor="transparent">
-                <h1>This is the navbar Component</h1>
-                {/* <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
+                <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                     <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
                     <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
-                </Navbar.Collapse> */}
+                </Navbar.Collapse>
             </Navbar>
         );
     }
