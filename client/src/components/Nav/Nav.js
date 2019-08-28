a3bac460126c8f828499a1042d6b3cadd3d6ad67
Greg Baker <semperfifcd@gmail.com>
2019-08-28 15:13:26 -0400

    Changed Nabar.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,13 +3,6 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import "./Style.css";
 
-<link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous"
-/>
-
 class Nav extends Component {
     render() {
         return (
