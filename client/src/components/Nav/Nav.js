61de66ea9502d0ae0605e711da0d771779384bc7
Greg Baker <semperfifcd@gmail.com>
2019-08-28 15:26:15 -0400

    Changed Nabar.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./Style.css";
 
 class Nav extends Component {
