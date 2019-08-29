dd32e8bd185a740279cc10842abc078b1558dd4e
unknown <ilyrodriguez@gmail.com>
2019-08-29 09:29:56 -0400

    about page working

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Nav from '../components/Navbar/Nav';
-import Footer from '../components/Footer/Footer';
 import logo from '../Images/logo.png';
 import formAboutNav from '../Images/formAboutNav.png';
 import './Style.css';
@@ -10,28 +9,50 @@ const About = () => {
         <div>
             <img src={formAboutNav} alt='' className='form-nav' />
             <Nav />
-            <div className="about-header">
-                <h3>Meet the juniors behind Junior Board</h3>
-                <div className="about-header-underline"></div>
-            </div>
-            {/* <img className='about-logo' alt='Junior Board' src={logo} /> */}
-            <div className='about'>
-                <div className='Greg'>
-                    <div className='picture'>
-                        <div className="image-placeholder"><img alt='Greg Baker' src='' className='Greg-photo' /></div>
-                    </div>
+            <div className='about-area'>
+                <div className='about-header'>
+                    <h3>Meet the juniors behind Junior Board</h3>
+                    <div className='about-header-underline'></div>
                 </div>
+                {/* <img className='about-logo' alt='Junior Board' src={logo} /> */}
+                <div className='about'>
+                    <div className='team-cards'>
+                        <div className='team-card-header'>
+                            <div className='image-placeholder-about'>
+                                <img alt='Ileana Mandigo' src='' className='' />
+                            </div>
+                            <h4 className="member-name">Ileana Mandigo</h4>
+                            <h6>Front-End Developer</h6>
+                            <h6>Phone: 407-259-7239</h6>
+                            <h6>ilyrodriguez@gmail.com</h6>
 
-                <div className='Ileana'>
-                    <div className='picture'>
-                    <div className="image-placeholder"><img alt='Ileana Mandigo' src=''/></div>
+                        </div>
                     </div>
-                </div>
 
-                <div className='Priscila'>
-                    <div className='picture'>
-                    <div className="image-placeholder"><img alt='Priscila Heller' src=''/></div>
+                    <div className='team-cards'>
+                        <div className='team-card-header'>
+                            <div className='image-placeholder-about'>
+                                <img alt='Greg Baker' src='' />
+                            </div>
+                            <h4 className="member-name">Greg Baker</h4>
+                            <h6>Back-End Developer</h6>
+                            <h6>Phone: 407-259-7239</h6>
+                            <h6>Semperfifcd@gmail.com</h6>
+                        </div>
                     </div>
+
+                    <div className='team-cards'>
+                        <div className='team-card-header'>
+                            <div className='image-placeholder-about'>
+                                <img alt='Priscila Heller' src='' />
+                            </div>
+                            <h4 className="member-name">Priscila Heller</h4>
+                            <h6>Google Authentication</h6>
+                            <h6>Phone: 407-259-7239</h6>
+                            <h6>Semperfifcd@gmail.com</h6>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
