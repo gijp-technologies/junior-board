23cef00a3ce5362cb9e9ebe7328501bf9bb62d7c
Greg Baker <semperfifcd@gmail.com>
2019-08-29 11:55:13 -0400

    Added edit form import.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main";
 import Form from "./Pages/Form";
 import About from "./Pages/About";
+import EditForm from "./Pages/EditForm";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
