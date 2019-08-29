916dca83b36979d9cd5d9a634dc279d79831f6ee
unknown <ilyrodriguez@gmail.com>
2019-08-29 11:37:16 -0400

    about done

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -28,41 +28,45 @@ const About = () => {
                                 <h6>ilyrodriguez@gmail.com</h6>
                             </div>
                         </div>
-                        <div><p>Ileana is a graphic designer with a passion for creativity, arts, and design.
-                            Her responsabilities for this projects were:</p>
-                        <ul>
-                        <li>UI/UX design</li>
-                        <li>Logo</li>
-                        
-                        </ul>
+                        <div className="responsabilies">
+                            <p>Ileana is a graphic designer with a passion for creativity, arts, and design.
+                            She worked on the Front-end process of this project: UI/UX design and logo; using React, Bootstrap and Photoshop.</p>
+    
                         </div>
-                        <div className="member-footer">
-                            <a href="https://www.linkedin.com/in/ileana-mandigo/" target="blank">LinkedIn</a>
-                            <a href="https://github.com/ilyrodriguez" target="blank">Github</a>
-                            <a href="https://ilyrodriguez.github.io/portfolio/" target="blank">Portfolio</a>
-                        </div>
-                    </div>
-
-                    <div className='team-cards'>
-                        <div className='team-card-header'>
-                            <div className='image-placeholder-about'>
-                                <img alt='Greg Baker' src={require('../Images/greg.jpg')} className='ileana-pic' />
-                            </div>
-                            <div className="member-info">
-                                <h4 >Greg Baker</h4>
-                                <h6>Back-End Developer</h6>
-                                <h6>Phone: 407-259-7239</h6>
-                                <h6>semperfifcd@gmail.com</h6>
+                            <div className="member-footer">
+                                <a href="https://www.linkedin.com/in/ileana-mandigo/" target="blank">LinkedIn</a>
+                                <a href="https://github.com/ilyrodriguez" target="blank">Github</a>
+                                <a href="https://ilyrodriguez.github.io/portfolio/" target="blank">Portfolio</a>
                             </div>
                         </div>
-                        <div className="member-footer">
-                            <a href="https://www.linkedin.com/in/greg-baker-48ab9717b/" target="blank">LinkedIn</a>
-                            <a href="https://github.com/BakedALake" target="blank">Github</a>
-                            <a href="https://bakedalake.github.io/BakedALake/portfolio.html" target="blank">Portfolio</a>
-                        </div>
-                    </div>
 
-                    {/* <div className='team-cards'>
+                        <div className='team-cards'>
+                            <div className='team-card-header'>
+                                <div className='image-placeholder-about'>
+                                    <img alt='Greg Baker' src={require('../Images/greg.jpg')} className='ileana-pic' />
+                                </div>
+                                <div className="member-info">
+                                    <h4 >Greg Baker</h4>
+                                    <h6>Back-End Developer</h6>
+                                    <h6>Phone: 407-259-7239</h6>
+                                    <h6>semperfifcd@gmail.com</h6>
+                                </div>
+                            </div>
+                            <div className="responsabilies">
+                            <p>Greg has highly analitical skills and passion for technology.
+                                He worked on the Back-end process of this project: Routes, Database, Deployment and Authentication;
+                                using MongoDB, Heroku and NodeJs.
+                            </p>
+    
+                        </div>
+                            <div className="member-footer">
+                                <a href="https://www.linkedin.com/in/greg-baker-48ab9717b/" target="blank">LinkedIn</a>
+                                <a href="https://github.com/BakedALake" target="blank">Github</a>
+                                <a href="https://bakedalake.github.io/BakedALake/portfolio.html" target="blank">Portfolio</a>
+                            </div>
+                        </div>
+
+                        {/* <div className='team-cards'>
                         <div className='team-card-header'>
                             <div className='image-placeholder-about'>
                                 <img alt='Priscila Heller' src='' />
@@ -74,10 +78,10 @@ const About = () => {
                         </div>
                     </div> */}
 
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
-
-export default About;
+            );
+        };
+        
+        export default About;
