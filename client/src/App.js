1acb31c5a1088b806dd7772cfddc52a5b4bb4b9c
Greg Baker <semperfifcd@gmail.com>
2019-08-29 11:42:18 -0400

    Enabled the edit form.

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route exact path="/form" component={Form} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/update/:email" component={EditForm} />
       </Router>
   );
 }
