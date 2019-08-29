98ec5cdb24ec74b69a931ada58719626c471ccf7
Greg Baker <semperfifcd@gmail.com>
2019-08-28 20:45:10 -0400

    Added Iliana's navbar code back in to test react-bootstrap.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,14 +6,14 @@ class Nav extends Component {
     render() {
         return (
 <h1>This is a navbar Component.</h1>
-            // <Navbar className="nav" backgroundColor="transparent">
-            //     <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
-            //     <Navbar.Toggle />
-            //     <Navbar.Collapse className="justify-content-end">
-            //              <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
-            //              <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
-            //     </Navbar.Collapse>
-            // </Navbar>
+            <Navbar className="nav" backgroundColor="transparent">
+                <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                         <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
+                         <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
