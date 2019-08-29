f075e96deb9c7780cd2e43b109c94558ec5258b5
unknown <ilyrodriguez@gmail.com>
2019-08-29 10:17:22 -0400

    about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -19,29 +19,32 @@ const About = () => {
                     <div className='team-cards'>
                         <div className='team-card-header'>
                             <div className='image-placeholder-about'>
-                                <img alt='Ileana Mandigo' src='' className='' />
+                                <img alt='Ileana Mandigo' src={require('../Images/ileana.jpg')} className='ileana-pic' />
                             </div>
-                            <h4 className="member-name">Ileana Mandigo</h4>
+                            <div className="member-info">
+                            <h4>Ileana Mandigo</h4>
                             <h6>Front-End Developer</h6>
                             <h6>Phone: 407-259-7239</h6>
                             <h6>ilyrodriguez@gmail.com</h6>
-
+                            </div>
                         </div>
                     </div>
 
                     <div className='team-cards'>
                         <div className='team-card-header'>
                             <div className='image-placeholder-about'>
-                                <img alt='Greg Baker' src='' />
+                                <img alt='Greg Baker' src={require('../Images/greg.jpg')} className='ileana-pic'/>
                             </div>
-                            <h4 className="member-name">Greg Baker</h4>
+                            <div className="member-info">
+                            <h4 >Greg Baker</h4>
                             <h6>Back-End Developer</h6>
                             <h6>Phone: 407-259-7239</h6>
                             <h6>semperfifcd@gmail.com</h6>
+                            </div> 
                         </div>
                     </div>
 
-                    <div className='team-cards'>
+                    {/* <div className='team-cards'>
                         <div className='team-card-header'>
                             <div className='image-placeholder-about'>
                                 <img alt='Priscila Heller' src='' />
@@ -49,9 +52,9 @@ const About = () => {
                             <h4 className="member-name">Priscila Heller</h4>
                             <h6>Google Authentication</h6>
                             <h6>Phone: 407-259-7239</h6>
-                            <h6>priscila.x.p.r@gmail.com</h6>
+                            <h6 className="team-email">priscila.x.p.r@gmail.com</h6>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
