4a172d199a665e65c76a73b7fa3c3f948ef3e7c1
Greg Baker <semperfifcd@gmail.com>
2019-08-28 21:02:41 -0400

    Changed navbar to a simple bootstarp button.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,14 +6,9 @@ import "./Style.css";
 
 function Nav() {
     return (
-        <Navbar className="nav" backgroundColor="transparent">
-            <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-                <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
-                <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
-            </Navbar.Collapse>
-        </Navbar>
+        <div>
+            <Button variant="primary">Primary</Button>
+        </div>
     );
 }
 
