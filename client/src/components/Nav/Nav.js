b1239695487f3957f6f882e32788bfbef653ff80
Greg Baker <semperfifcd@gmail.com>
2019-08-28 20:48:07 -0400

    Navbar work!

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,13 +5,12 @@ import "./Style.css";
 class Nav extends Component {
     render() {
         return (
-<h1>This is a navbar Component.</h1>
             <Navbar className="nav" backgroundColor="transparent">
                 <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                         <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
-                         <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
+                    <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
+                    <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
                 </Navbar.Collapse>
             </Navbar>
         );
