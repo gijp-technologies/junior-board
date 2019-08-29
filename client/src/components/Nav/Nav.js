14a8698db062a7e830a9b75c15dc06185744bd10
Greg Baker <semperfifcd@gmail.com>
2019-08-28 20:57:13 -0400

    Made navbar component stateless.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react'
 import { Navbar, Button } from "react-bootstrap";
 import "./Style.css";
 
-class Nav extends Component {
-    render() {
-        return (
-            <Navbar className="nav" backgroundColor="transparent">
-                <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
-                    <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+
+
+function Nav() {
+    return (
+        <Navbar className="nav" backgroundColor="transparent">
+            <Navbar.Brand href="#home"><img id="nav-logo" alt="Jr Board" src={require('../../images/LogoNav.png')}></img></Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Button variant="outline-light" className="google-login nav-btn" href="#login">Login</Button>
+                <Button variant="outline-light" className="nav-btn" href="#About">About</Button>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
 export default Nav;
