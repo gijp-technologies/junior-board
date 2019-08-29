30eb9bb8b59bcba00de66418f59d8af21e6d1e87
unknown <ilyrodriguez@gmail.com>
2019-08-29 08:24:25 -0400

    working on about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import Nav from '../components/Navbar/Nav';
 import Footer from '../components/Footer/Footer';
-import juniorboardlogo from '../Images/juniorboardlogo.png';
-import formAboutNav from '../Images/formAboutNav.png';  
+import logo from '../Images/logo.png';
+import formAboutNav from '../Images/formAboutNav.png';
 import './Style.css';
 
 const About = () => {
     return (
         <div>
-            <img src={formAboutNav} alt="" className="form-nav" />
+            <img src={formAboutNav} alt='' className='form-nav' />
             <Nav />
-            <img className='jr-board-about' alt='Junior Board' src={juniorboardlogo} />
+            <div className="about-header">
+                <h3>Meet the juniors behind Junior Board</h3>
+                <div className="about-header-underline"></div>
+            </div>
+            {/* <img className='about-logo' alt='Junior Board' src={logo} /> */}
             <div className='about'>
                 <div className='Greg'>
-                    <div className="picture">
-                        < img alt="Greg Baker" src="" className="Greg-photo"></img>
+                    <div className='picture'>
+                        <div className="image-placeholder"><img alt='Greg Baker' src='' className='Greg-photo' /></div>
                     </div>
-                    <div className="picture">
-                        <div className='Ileana'>
+                </div>
 
-                            <img alt="Ileana Mandigo" src=""></img>
-                        </div>
-                        <div className='Priscila'>
-                            <img alt="Priscila Heller" src=""></img>
-                        </div>
+                <div className='Ileana'>
+                    <div className='picture'>
+                    <div className="image-placeholder"><img alt='Ileana Mandigo' src=''/></div>
+                    </div>
+                </div>
+
+                <div className='Priscila'>
+                    <div className='picture'>
+                    <div className="image-placeholder"><img alt='Priscila Heller' src=''/></div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
 
 export default About;
